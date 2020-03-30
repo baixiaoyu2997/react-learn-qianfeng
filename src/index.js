120edc398a2c5ca3006beca9cab3395068e3d88e
L.Rain <bxy2997@sina.com>
2020-03-30 21:41:50 +0800

    22：react-hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { useState,useEffect } from "react";
+import ReactDOM from "react-dom";
+// import App from "./App";
 
+const Counteer = () => {
+  const [count, setCount] = useState(0);
+  useEffect(()=>{
+    console.log('更新了')
+  })
+  return (
+    <div>
+      <button onClick={()=>{setCount(count-1)}}>-</button>
+      <span>{count}</span>
+      <button onClick={()=>{setCount(count+1)}}>+</button>
+    </div>
+  );
+};
 ReactDOM.render(
   <React.StrictMode>
-    <App desc="类组件世纪城React.Component的"/>
+    {/* <App desc="类组件世纪城React.Component的"/> */}
+    <Counteer></Counteer>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
