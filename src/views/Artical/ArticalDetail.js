d77c7b2db354130e4296b8e41bf5fdac889b9852
L.Rain <bxy2997@sina.com>
2020-04-01 15:15:47 +0800

    40：路由的一些常用API

--- src/views/Artical/ArticalDetail.js
+++ src/views/Artical/ArticalDetail.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import { BackButton } from "../../components";
 class ArticalDetail extends Component {
     render() {
         console.log(this.props)
         return (
             <div>
                 文章详情
+                <BackButton onClick={this.goHome}>返回首页</BackButton>
             </div>
         );
     }
