6bb1f9c5906c0339cc8330f5ff46dd0bf70fd275
L.Rain <bxy2997@sina.com>
2020-04-02 14:06:55 +0800

    50：项目-使用axios获取数据及表格的基本渲染

---
+++ src/requests/index.js
@@ -0,0 +1,27 @@
+import axios  from "axios";
+import { message } from "antd";
+const isDev=process.env.NODE_ENV==="development"
+const service=axios.create({
+    baseURL:isDev?'http://rap2.taobao.org:38080/app/mock/249448':''
+})
+service.interceptors.request.use((config)=>{
+    config.data=Object.assign({},config.data,{
+        authToken:'sfsdfsdfsdf'
+    })
+    // return Object.assign({},config.headers,{authToken:'sdfsd'})
+    return config
+})
+service.interceptors.response.use((resp)=>{
+    if(resp.data.code===200){
+        return resp.data.data
+    }else{
+        // 全局处理错误
+        message.error('错误')
+    }
+})
+export const getArticles=(offset=0,limit=10)=>{
+    return service.post('/api/v1/articlelist',{
+        offset,
+        limit
+    })
+}
--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,46 +1,59 @@
 import React, { Component } from "react";
 import { Card, Button, Table } from "antd";
+import {getArticles} from '../../requests'
 class Article extends Component {
-  dataSource = [
-    {
-      key: "1",
-      name: "胡彦斌",
-      age: 32,
-      address: "西湖区湖底公园1号"
-    },
-    {
-      key: "2",
-      name: "胡彦祖",
-      age: 42,
-      address: "西湖区湖底公园1号"
-    }
-  ];
-
-  columns = [
-    {
-      title: "姓名",
-      dataIndex: "name",
-      key: "name"
-    },
-    {
-      title: "年龄",
-      dataIndex: "age",
-      key: "age"
-    },
-    {
-      title: "住址",
-      dataIndex: "address",
-      key: "address"
-    }
-  ];
+  constructor(){
+      super()
+      this.state={
+        dataSource : [
+            {
+              key: "1",
+              name: "胡彦斌",
+              age: 32,
+              address: "西湖区湖底公园1号"
+            },
+            {
+              key: "2",
+              name: "胡彦祖",
+              age: 42,
+              address: "西湖区湖底公园1号"
+            }
+          ],
+          columns :[
+            {
+              title: "姓名",
+              dataIndex: "name",
+              key: "name"
+            },
+            {
+              title: "年龄",
+              dataIndex: "age",
+              key: "age"
+            },
+            {
+              title: "住址",
+              dataIndex: "address",
+              key: "address"
+            }
+          ],
+          total:0
+      }
+  }
   render() {
     return (
       <div>
         <Card title="文章列表" bordered={false} extra={<Button>导入</Button>}>
-          <Table dataSource={this.dataSource} columns={this.columns}></Table>
+          <Table dataSource={this.dataSource} columns={this.columns} pagination={{total:this.state.total,hideOnSinglePage:true}}></Table>
         </Card>
       </div>
     );
+  }
+  componentDidMount(){
+    getArticles().then((data)=>{
+        this.setState({
+            total:data.total
+        })
+    })
   }
 }
 
