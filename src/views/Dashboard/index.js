a1afcbd0beae9d377f3123c0af35bd1a6b35404d
L.Rain <bxy2997@sina.com>
2020-04-03 10:46:21 +0800

    第065集-项目-常见图表库介绍-在项目中使用echarts（上）

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Card, Row, Col } from "antd";
+import './dashboard.less'
 class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                仪表盘
-            </div>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Card title="概览" bordered={false}>
+          <Row gutter={16}>
+            <Col className="gutter-row" span={6}>
+              <div className="qf-gutter-box" style={{backgroundColor:'#29b6f6'}}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div className="qf-gutter-box" style={{backgroundColor:'#ab47bc'}}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div className="qf-gutter-box" style={{backgroundColor:'#ff7043'}}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div className="qf-gutter-box" style={{backgroundColor:'#43a047'}}>col-6</div>
+            </Col>
+          </Row>
+        </Card>
+        <Card title="最近浏览量" bordered={false}>
+
+        </Card>
+      </>
+    );
+  }
 }
 
 export default Dashboard;
