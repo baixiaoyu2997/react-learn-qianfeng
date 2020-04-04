64328ae669483703c338ffd4699f150fd174dbe5
L.Rain <bxy2997@sina.com>
2020-04-04 10:38:54 +0800

    第078集-项目-图片上传实现更换头像（上）

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -1,13 +1,49 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Card, Upload, Spin } from "antd";
+import axios from "axios";
 class Profile extends Component {
-    render() {
-        return (
-            <div>
-                个人设置页面
-            </div>
-        );
-    }
+  state = {
+    isUploading: false,
+    avatarUrl: "",
+  };
+  handleUploadAvatar = ({file}) => {
+    const data = new FormData();
+    data.append(
+      "Token",
+      "daa1633530031f6cd2fbfb18cd1d94cdcc990bab:uTGIBxVG1jOYzCviy3ylmwvgcjo=:eyJkZWFkbGluZSI6MTU4NTk2NzMzNCwiYWN0aW9uIjoiZ2V0IiwidWlkIjoiNzE1MTMwIiwiYWlkIjoiMTY3NzQ5NCIsImZyb20iOiJmaWxlIn0="
+    );
+    data.append("file", file);
+    this.setState({
+      isUploading: true,
+    });
+    axios.post("http://up.imgapi.com/", data).then((res) => {
+      console.log(res.data.linkurl);
+    });
+  };
+  render() {
+    return (
+      <Card title="个人设置" bordered={false}>
+        <Upload
+          style={{
+            border: '1px dashed #dedede',
+            width: 80,
+            height: 80,
+            display: 'block',
+          }}
+          showUploadList={false}
+          customRequest={this.handleUploadAvatar}
+        >
+          <Spin spinning={this.state.isUploading}>
+            {this.state.avatarUrl ? (
+              <img src={this.state.avatarUrl} alt="头像" />
+            ) : (
+              <span>点击上传</span>
+            )}
+          </Spin>
+        </Upload>
+      </Card>
+    );
+  }
 }
 
 export default Profile;
